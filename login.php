cbe632cc1fb12ae7ff154335e80df202c00cb68e
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-08-18 09:25:50 -0300

    Including Prepared Statements in php files to prevent SQL injections.

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
     include_once("db_connect.php");
-    
+
     $postdata = file_get_contents("php://input");
     
     if(isset($postdata) && !empty($postdata))
@@ -8,28 +8,41 @@
         $request = json_decode($postdata);
         $email = mysqli_real_escape_string($mysqli, trim($request->email));
         $senha = mysqli_real_escape_string($mysqli, trim($request->senha));
-
-        $sql = "SELECT * FROM usuarios where email='$email' and senha='$senha'";
-        $result = mysqli_query($mysqli, $sql);
+    
+        $sql = "SELECT * FROM usuarios WHERE email=? AND senha=?";
+        $stmt = mysqli_prepare($mysqli, $sql);
+        mysqli_stmt_bind_param($stmt, "ss", $email, $senha);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+    
         $numsUser = mysqli_num_rows($result);
-
-        if($numsUser>0)
+    
+        if($numsUser > 0)
         {
-            $data = array('message'=>'user success', 'email'=>$email);
+            $data = array('message' => 'user success', 'email' => $email);
             echo json_encode($data);
-        }else{
-            $sqla = "SELECT * FROM admins where email='$email' and senha='$senha'";
-            $result = mysqli_query($mysqli, $sqla);
+        }
+        else
+        {
+            $sqla = "SELECT * FROM admins WHERE email=? AND senha=?";
+            $stmt = mysqli_prepare($mysqli, $sqla);
+            mysqli_stmt_bind_param($stmt, "ss", $email, $senha);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+    
             $numsAdmin = mysqli_num_rows($result);
-
-            if($numsAdmin>0)
+    
+            if($numsAdmin > 0)
             {
-                $data = array('message'=>'admin success', 'email'=>$email);
+                $data = array('message' => 'admin success', 'email' => $email);
                 echo json_encode($data);
-            }else{
-                $data = array('message'=>'failed');
+            }
+            else
+            {
+                $data = array('message' => 'failed');
                 echo json_encode($data);
             }
         }
     }
+    
 ?>
--- register.php
+++ register.php
@@ -1,37 +1,37 @@
 <?php
     include_once("db_connect.php");
-    
+
     $postdata = file_get_contents("php://input");
     
     if(isset($postdata) && !empty($postdata))
     {
         $request = json_decode($postdata);
         $nome = trim($request->nome);
-        $cpf = mysqli_real_escape_string($mysqli, trim($request->cpf));
-        $email = mysqli_real_escape_string($mysqli, trim($request->email));
-        $telefone = mysqli_real_escape_string($mysqli, trim($request->telefone));
-        $senha = mysqli_real_escape_string($mysqli, trim($request->senha));
-
+        $cpf = trim($request->cpf);
+        $email = trim($request->email);
+        $telefone = trim($request->telefone);
+        $senha = trim($request->senha);
+    
         $sql = "INSERT INTO usuarios(
             nome, 
             cpf, 
             email, 
             telefone, 
             senha
-        ) VALUES (
-            '$nome', 
-            '$cpf', 
-            '$email', 
-            '$telefone', 
-            '$senha'
-        )";
-
-        if ($mysqli->query($sql)){
+        ) VALUES (?, ?, ?, ?, ?)";
+    
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("sssss", $nome, $cpf, $email, $telefone, $senha);
+    
+        if ($stmt->execute()){
             $data = array('message'=>'success');
             echo json_encode($data);
         } else{
             $data = array('message'=>'failed');
             echo json_encode($data);
         }
+    
+        $stmt->close();
     }
+    
 ?>
