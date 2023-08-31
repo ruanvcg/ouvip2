2e7878d3c1133f4c18cc8783b04c32696da1c273
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-08-31 09:43:49 -0300

    [FEAT] Organizing routes to track reporting statuses.

---
+++ OuviP2/php/report/view_forwarded_reports.php
@@ -0,0 +1,21 @@
+<?php
+    // include database connection
+    include '../db_connect.php';
+    
+    // delete message prompt will be here
+    
+    // select all data
+    $query = "SELECT id, tipoReporte, categoria, nome FROM reportes WHERE statusReporte = 'Encaminhado' ORDER BY id DESC";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    
+    $result = $stmt->get_result(); // Get the mysqli_result object
+    
+    $results = [];
+    while ($row = $result->fetch_assoc()) {
+        $results[] = $row;
+    }
+
+    $json = json_encode($results);
+    echo $json;
+?>
