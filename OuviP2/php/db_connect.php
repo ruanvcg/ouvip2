176c0f57c89f9606c3b4d53f7c838b039bfb4022
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-09-06 12:18:16 -0300

    [TEST] Migrating the database to alwaysdata.

--- OuviP2/php/db_connect.php
+++ OuviP2/php/db_connect.php
@@ -4,13 +4,16 @@
     header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header("Content-Type: application/json; charset=UTF-8");
+    use Dotenv\Dotenv as Dotenv;
     $config = include('config.php');
 
+
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
     //Credentials
-    $db_host = $config['db_host'];
-    $db_username = $config['db_username'];
-    $db_password = $config['db_password'];
-    $db_name = $config['db_name'];
+    $db_host = $_ENV['HOST'];
+    $db_username = $_ENV['USER'];
+    $db_password = $_ENV['PASS'];
+    $db_name = $_ENV['NAME'];
 
     //Connection
     $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
