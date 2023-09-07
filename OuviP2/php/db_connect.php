9f7b87bad244aa5c43ed0ffb5a0b682afa9739c3
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-09-07 10:34:36 -0300

    [TEST] Changing the conection credentials.

--- OuviP2/php/db_connect.php
+++ OuviP2/php/db_connect.php
@@ -4,14 +4,13 @@
     header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header("Content-Type: application/json; charset=UTF-8");
-    $config = include('config.php');
 
 
     // Credentials
     $db_host = 'mysql-ruan.alwaysdata.net';
     $db_username = 'ruan';
     $db_password = 'tccruanouvip22023';
-    $db_name = 'ruan_db';
+    $db_name = 'ruan_ouvidb';
 
     //Connection
     $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
