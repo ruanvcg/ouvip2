8b26d7ffeb9a687b18c2925b449d42121010b57a
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-09-07 10:03:46 -0300

    [TEST] Changing the conection credentials.

--- OuviP2/php/db_connect.php
+++ OuviP2/php/db_connect.php
@@ -4,16 +4,14 @@
     header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header("Content-Type: application/json; charset=UTF-8");
-    use Dotenv\Dotenv as Dotenv;
     $config = include('config.php');
 
 
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-    //Credentials
-    $db_host = $_ENV['HOST'];
-    $db_username = $_ENV['USER'];
-    $db_password = $_ENV['PASS'];
-    $db_name = $_ENV['NAME'];
+    // Credentials
+    $db_host = 'mysql-ruan.alwaysdata.net';
+    $db_username = 'ruan';
+    $db_password = 'tccruanouvip22023';
+    $db_name = 'ruan_db';
 
     //Connection
     $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
